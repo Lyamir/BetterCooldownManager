44638374dd206ecf46c59aee83729c34ffbafe00
Dale Hunt <dalehunt@gmail.com>
2026-01-19 10:31:37 +0000

    maybe just do this

--- Core/Core.lua
+++ Core/Core.lua
@@ -1,14 +1,6 @@
 local _, BCDM = ...
 local BetterCooldownManager = LibStub("AceAddon-3.0"):NewAddon("BetterCooldownManager")
 
-local SUPPORTED_ADDONS = {
-    [1] = "BetterBlizzardFrames",
-    [2] = "Cell",
-    [3] = "ElvUI",
-    [4] = "MidnightSimpleUnitFrames",
-    [5] = "UnhaltedUnitFrames",
-}
-
 function BetterCooldownManager:OnInitialize()
     BCDM.db = LibStub("AceDB-3.0"):New("BCDMDB", BCDM:GetDefaultDB(), true)
     BCDM.LDS:EnhanceDatabase(BCDM.db, "UnhaltedUnitFrames")
@@ -28,14 +20,10 @@ function BetterCooldownManager:OnEnable()
     BCDM:CreatePowerBar()
     BCDM:CreateSecondaryPowerBar()
     BCDM:CreateCastBar()
-    for _, AddOn in ipairs(SUPPORTED_ADDONS) do
-        EventUtil.ContinueOnAddOnLoaded(AddOn, function()
-            BCDM:SetupCustomCooldownViewer()
-            BCDM:SetupAdditionalCustomCooldownViewer()
-            BCDM:SetupCustomItemBar()
-            BCDM:SetupTrinketBar()
-        end)
-    end
+    BCDM:SetupCustomCooldownViewer()
+    BCDM:SetupAdditionalCustomCooldownViewer()
+    BCDM:SetupCustomItemBar()
+    BCDM:SetupTrinketBar()
     BCDM:CreateCooldownViewerOverlays()
     BCDM:SetupEditModeManager()
 end
